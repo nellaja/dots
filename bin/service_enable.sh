803c1af82d8b171a73684c6951214627831c45a2
nellaja <59583786+nellaja@users.noreply.github.com>
2024-11-19 06:35:16 -0500

    Update service_enable.sh

--- bin/service_enable.sh
+++ bin/service_enable.sh
@@ -9,6 +9,7 @@ clear
 sudo systemctl daemon-reload
 sudo systemctl start /dev/zram0
 
-# Enable System Services
-sudo systemctl enable avahi-daemon bluetooth cups firewalld systemd-timesyncd fstrim.timer
+# Enable System Services and Timers
+sudo systemctl enable avahi-daemon bluetooth cups firewalld systemd-timesyncd 
+sudo systemctl enable fstrim.timer paccache.timer pkgfile-update.timer
 systemctl --user enable pipewire.socket pipewire-pulse.socket wireplumber
