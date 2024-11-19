7cf12007971131f718a56d9aea269bf832fd1d2c
nellaja <59583786+nellaja@users.noreply.github.com>
2024-11-18 21:16:17 -0500

    Update mirror_update.sh

--- bin/mirror_update.sh
+++ bin/mirror_update.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-set -e
-shopt -s extglob
-shopt -s lastpipe
+set -euo pipefail
+IFS=$'\n\t'
+
+clear
 
 sudo reflector --verbose --protocol https --country US,UK,Canada,France,Germany --latest 19 --score 11 --sort rate --save /etc/pacman.d/mirrorlist
